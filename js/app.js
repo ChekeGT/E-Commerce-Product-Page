4c83f85dd54ad453035b4bdf5bf25428c7087a36
Cheke <chekelosos@gmail.com>
2023-10-25 14:51:06 -0600

    Added functionality and styles for hamburger-button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
+// Hamburger button functionality
+function getAllParents(child){
+    let parentElements = []
+    
+    let parent_element = child.parentNode
+    while (parent_element){
+        parentElements.push(parent_element)
+        parent_element = parent_element.parentNode
+    }
+    return parentElements
+}
+function isInHtmlCollection(c, e){
+    for (let i = 0; i < c.length; i++){
+        let element = c[i]
+        if (e == element){
+            return true
+        }
+    }
+    return false
+}
+function blur_everything_except_links_container(links_container){
+    // element should not be the link container check
+    // it shouldnt be a parent
+    // it shouldnt be a child, children: li &a 
+    const allElements = document.querySelectorAll('*')
+    const parents = getAllParents(hamburger_button)
+    const children = document.querySelectorAll('.pages-links-container *')
+    allElements.forEach( element => {
+        if ((element != links_container && !parents.includes(element) && !isInHtmlCollection(children, element))){
+            element.classList.add('blur')
+        }
+    })
+}
+function unblur_everything(){
+    const allElements = document.querySelectorAll('*')
+    allElements.forEach(element => {
+        element.classList.remove('blur')
+    })
+}
+const hamburger_button = document.querySelector('#hamburger-button')
+
+const links_div = document.querySelector('.pages-links-container')
+
+
+
+hamburger_button.addEventListener('click', function(e){
+    e.preventDefault()
+    links_div.classList.add('reveal-hidden-links')
+    blur_everything_except_links_container(links_div)
+    e.stopPropagation()
+})
+
+const body = document.body
+
+links_div.addEventListener('click', function(e){
+    e.stopPropagation()
+})
+body.addEventListener('click', function(e){
+    unblur_everything()
+    links_div.classList.remove('reveal-hidden-links')
+})
+
 // This functionality determines how many items you will add to your cart.
 
 const stock_selector_div = document.querySelector('.stock-selector')
@@ -13,13 +75,11 @@ stock_selector_div.addEventListener('click', function(e){
     // That means substracting or adding
     const target = e.target
     if (target.className == 'minus-button'){
-        console.log('hola')
         if (!(counter.textContent == '0')){
             counter.textContent = parseInt(counter.textContent) - 1
         }
     }
     if (target.className == 'plus-button'){
-        console.log('hola')
         counter.textContent = parseInt(counter.textContent) + 1
     }
 })
